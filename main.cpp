d0ee31d3535e2edb93454731a2132ae0484a6f9d
Muhammadjon Kurbonov <muhsmmad123321@gmail.com>
2025-02-07 10:49:52 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,14 +8,12 @@ int main() {
     cout << "Hamming Code Program (8/4)" << endl;
     cout << "==========================" << endl;
 
-    // Ввод 4 информационных битов
     cout << "Enter 4 data bits (0 or 1): ";
     vector<int> data(4);
     for (int i = 0; i < 4; i++) {
         cin >> data[i];
     }
 
-    // Вызов функции работы с кодом Хэмминга
     hamming_code(data);
 
     return 0;
